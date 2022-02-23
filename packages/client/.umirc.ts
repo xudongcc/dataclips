8508856323a60034d2e5aaffb27494abc32c9446
d4rkcr0w <me@d4rkcr0w.com>
2022-02-23 11:25:04 +0800

    feat: 添加图标，拆分前端包

--- packages/client/.umirc.ts
+++ packages/client/.umirc.ts
@@ -5,6 +5,7 @@ import routes from './routes';
 const { SERVER_URL } = process.env;
 
 export default defineConfig({
+  favicon: '/favicon.png',
   routes,
   proxy: {
     '/graphql': {
@@ -17,10 +18,34 @@ export default defineConfig({
     },
   },
   webpack5: {},
+  dynamicImport: {
+    loading: '@/pages/Loading',
+  },
+  chunks: ['vendors', 'umi'],
   chainWebpack(config) {
     config.module
       .rule('mjs-rule')
       .test(/.m?js/)
       .resolve.set('fullySpecified', false);
+
+    config.merge({
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          minSize: 30000,
+          minChunks: 3,
+          automaticNameDelimiter: '.',
+          cacheGroups: {
+            vendor: {
+              name: 'vendors',
+              test({ resource }: { resource: string }) {
+                return /[\\/]node_modules[\\/]/.test(resource);
+              },
+              priority: 10,
+            },
+          },
+        },
+      },
+    });
   },
 });
---
+++ packages/client/src/pages/Loading.tsx
@@ -0,0 +1,12 @@
+import { Flex, Spinner } from '@chakra-ui/react';
+import { FC } from 'react';
+
+const Loading: FC = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Spinner />
+    </Flex>
+  );
+};
+
+export default Loading;
