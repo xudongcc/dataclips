6e7e695d7258ad687eae5bc3790fbd30e365770e
chenxuanzong <2622009279@qq.com>
2022-02-24 13:02:41 +0800

    feat: react-table 添加排序同步功能

--- packages/client/src/pages/ClipPreview.tsx
+++ packages/client/src/pages/ClipPreview.tsx
@@ -10,7 +10,28 @@ const ClipPreview: FC = () => {
   const { slug } = useParams<{ slug: string }>();
 
   const { data: result, isLoading } = useQuery(['result', slug], () =>
-    fetch(`/clips/${slug}.json`).then((res) => res.json()),
+    fetch(`/clips/${slug}.json`)
+      .then((res) => res.json())
+      .then((res) => {
+        // table 的所需要的数据
+        res.tableValues = res.values.map((value: Array<Array<any>>) => {
+          const item: Record<string, any> = {};
+
+          res.fields.forEach((key: string, index: number) => {
+            item[key] = value[index];
+          });
+
+          return item;
+        });
+
+        // 生成 columns
+        res.columns = res.fields.map((value: string) => ({
+          Header: value,
+          accessor: value,
+        }));
+
+        return res;
+      }),
   );
 
   if (isLoading) {
