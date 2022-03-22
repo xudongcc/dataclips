f8f6eeb943e1b4f218bbca7cc2ed2e5988b2814e
d4rkcr0w <me@d4rkcr0w.com>
2022-03-22 12:51:34 +0800

    fix: next.config.js

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -7,18 +6,22 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: "/api/graphql",
-        destination: `${process.env.SERVER_URL}/api/graphql`,
-      },
-      {
-        source: "/clips/:path.csv",
-        destination: `${process.env.SERVER_URL}/clips/:path.csv`,
-      },
-      {
-        source: "/clips/:path.json",
-        destination: `${process.env.SERVER_URL}/clips/:path.json`,
-      },
+      ...(process.env.SERVER_URL
+        ? [
+            {
+              source: "/api/graphql",
+              destination: `${process.env.SERVER_URL}/api/graphql`,
+            },
+            {
+              source: "/clips/:path.csv",
+              destination: `${process.env.SERVER_URL}/clips/:path.csv`,
+            },
+            {
+              source: "/clips/:path.json",
+              destination: `${process.env.SERVER_URL}/clips/:path.json`,
+            },
+          ]
+        : []),
     ];
   },
   webpack(config, { isServer }) {
