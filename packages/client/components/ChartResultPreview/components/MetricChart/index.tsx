7bb1668a0db2ad8f9362cde58e2a080aed195f51
d4rkcr0w <me@d4rkcr0w.com>
2022-04-01 10:07:24 +0800

    fix: 优化指标图显示

--- packages/client/components/ChartResultPreview/components/MetricChart/index.tsx
+++ packages/client/components/ChartResultPreview/components/MetricChart/index.tsx
@@ -1,9 +1,18 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { FC, useMemo } from "react";
+import { FiArrowDownRight, FiArrowUpRight } from "react-icons/fi";
 
 import { ResultFragment } from "../../../../generated/graphql";
 import { getFormatValue } from "../../../ChartEditTab/components/FormatFieldForm";
-import { Stat } from "../../../Stat";
 
 export interface MetricChartConfig {
   valueCol: string;
@@ -20,46 +29,62 @@ export const MetricChartPreview: FC<MetricChartPreviewProps> = ({
   result,
   config = { valueCol: "", compareCol: "", format: "" },
 }) => {
-  const component = useMemo(() => {
-    const stat = {
-      label: "",
-      value: "",
-      delta: { value: "", isUpwardsTrend: undefined },
-    };
-
+  const value = useMemo(() => {
     const valueColIndex = result.fields.findIndex(
       (value) => value === config.valueCol
     );
 
+    if (valueColIndex > 0) {
+      return getFormatValue(result.values[0]?.[valueColIndex], config.format);
+    }
+
+    return null;
+  }, [result, config]);
+
+  const compareValue = useMemo(() => {
     const compareColIndex = result.fields.findIndex(
       (c) => c === config.compareCol
     );
 
-    if (valueColIndex !== -1) {
-      stat.value = getFormatValue(
-        result.values[0]?.[valueColIndex],
-        config.format
-      );
+    if (compareColIndex > 0) {
+      return getFormatValue(result.values[0]?.[compareColIndex], config.format);
     }
 
-    if (valueColIndex !== -1 && compareColIndex !== -1) {
-      stat.delta = {
-        value: getFormatValue(
-          result.values[0]?.[compareColIndex],
-          config.format
-        ),
-        isUpwardsTrend:
-          getFormatValue(result.values[0]?.[valueColIndex], config.format) >=
-          getFormatValue(result.values[0]?.[compareColIndex], config.format),
-      };
-    }
-
-    return <Stat {...stat} />;
+    return null;
   }, [result, config]);
+
+  const isUpwardsTrend = useMemo(() => {
+    return compareValue && value && value > compareValue;
+  }, [value, compareValue]);
 
   return (
     <Flex h="100%" justify="center" align="center">
-      {component}
+      <Box
+        px={{ base: "4", md: "6" }}
+        py={{ base: "5", md: "6" }}
+        borderRadius="lg"
+      >
+        <Stack>
+          <Stack spacing="4">
+            <Heading size={useBreakpointValue({ base: "sm", md: "md" })}>
+              {value}
+            </Heading>
+
+            {compareValue ? (
+              <HStack spacing="1" fontWeight="medium">
+                <Icon
+                  color={isUpwardsTrend ? "success" : "error"}
+                  as={isUpwardsTrend ? FiArrowUpRight : FiArrowDownRight}
+                  boxSize="5"
+                />
+                <Text color={isUpwardsTrend ? "success" : "error"}>
+                  {compareValue}
+                </Text>
+              </HStack>
+            ) : null}
+          </Stack>
+        </Stack>
+      </Box>
     </Flex>
   );
 };
