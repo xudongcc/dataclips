7054680432d61f900576f2d35b597c10ff3c8aad
chenxuanzong <48152698+chenxuanzong@users.noreply.github.com>
2022-04-01 14:54:34 +0800

    fix: 大小写修改 (#53)

---
+++ packages/client/components/chart/ChartEditTab/index.tsx
@@ -0,0 +1,170 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  FormControl,
+  Select,
+  Text,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { FC } from "react";
+
+import { ResultFragment } from "../../../generated/graphql";
+import { ChartServerConfig, ChartType } from "../../../types";
+import {
+  FunnelChartConfigForm,
+  MetricChartConfigForm,
+  BarChartConfigForm,
+  LineChartConfigForm,
+  PieChartConfigForm,
+} from "./components";
+import { FormatFieldForm } from "./components/FormatFieldForm";
+
+export const chartTypeMap = {
+  [ChartType.FUNNEL]: "漏斗图",
+  [ChartType.METRIC]: "指标图",
+  [ChartType.LINE]: "折线图",
+  [ChartType.BAR]: "柱状图",
+  [ChartType.PIE]: "饼图",
+};
+
+interface ChartEditTabProps extends Partial<ChartServerConfig> {
+  result?: ResultFragment;
+  form: any;
+}
+
+export const ChartEditTab: FC<ChartEditTabProps> = ({
+  result = { fields: [], values: [] },
+  form,
+}) => {
+  const router = useRouter();
+
+  const { chartId } = router.query as { chartId: string };
+
+  return (
+    <Accordion defaultIndex={[chartId ? 2 : 0]} allowToggle>
+      <AccordionItem>
+        <AccordionButton>
+          <Text fontWeight="bold" flex="1" textAlign="left">
+            图表类型
+          </Text>
+          <AccordionIcon />
+        </AccordionButton>
+
+        <AccordionPanel pb={4}>
+          <FormControl isInvalid={!!form.errors.type}>
+            <Select
+              size="sm"
+              placeholder="请选择图表分类"
+              name="type"
+              onChange={form.handleChange}
+              value={form.values.type}
+            >
+              {[
+                ChartType.FUNNEL,
+                ChartType.METRIC,
+                ChartType.LINE,
+                ChartType.BAR,
+                ChartType.PIE,
+              ].map((item) => (
+                <option value={item} key={item}>
+                  {chartTypeMap[item]}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem isDisabled={!form.values?.type}>
+        <AccordionButton>
+          <Text fontWeight="bold" flex="1" textAlign="left">
+            标准配置
+          </Text>
+          <AccordionIcon />
+        </AccordionButton>
+
+        <AccordionPanel pb={4}>
+          <FormatFieldForm form={form} />
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem isDisabled={!form.values?.type}>
+        <AccordionButton>
+          <Text fontWeight="bold" flex="1" textAlign="left">
+            查询分析配置
+          </Text>
+          <AccordionIcon />
+        </AccordionButton>
+        <AccordionPanel pb={4}>
+          {
+            [
+              {
+                type: ChartType.FUNNEL,
+                component: (
+                  <FunnelChartConfigForm
+                    form={form}
+                    editOptionConfig={{
+                      groupCol: result.fields,
+                      valueCol: result.fields,
+                    }}
+                  />
+                ),
+              },
+              {
+                type: ChartType.METRIC,
+                component: (
+                  <MetricChartConfigForm
+                    form={form}
+                    editOptionConfig={{
+                      valueCol: result.fields,
+                      compareCol: result.fields,
+                    }}
+                  />
+                ),
+              },
+              {
+                type: ChartType.LINE,
+                component: (
+                  <LineChartConfigForm
+                    form={form}
+                    editOptionConfig={{
+                      xCol: result.fields,
+                      yCol: result.fields,
+                    }}
+                  />
+                ),
+              },
+              {
+                type: ChartType.BAR,
+                component: (
+                  <BarChartConfigForm
+                    form={form}
+                    editOptionConfig={{
+                      xCol: result.fields,
+                      yCol: result.fields,
+                    }}
+                  />
+                ),
+              },
+              {
+                type: ChartType.PIE,
+                component: (
+                  <PieChartConfigForm
+                    form={form}
+                    editOptionConfig={{
+                      keys: result.fields,
+                      values: result.fields,
+                    }}
+                  />
+                ),
+              },
+            ].find((item) => item.type === form.values?.type)?.component
+          }
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+};
---
+++ packages/client/components/chart/ChartResultPreview/components/PieChart/index.tsx
@@ -0,0 +1,126 @@
+import {
+  Chart,
+  Coordinate,
+  DonutChart,
+  Interaction,
+  Interval,
+  Tooltip,
+} from "bizcharts";
+import { sumBy } from "lodash";
+import { FC, useMemo } from "react";
+import { ResultFragment } from "../../../../../generated/graphql";
+import { formatPercent } from "../../../../../utils/formatPercent";
+import { getFormatValue } from "../../../ChartEditTab/components/FormatFieldForm";
+
+export interface PieChartConfig {
+  variant: string;
+  key: string;
+  value: string;
+  format: string;
+}
+
+interface PieChartPreviewProps {
+  result: ResultFragment;
+  config: PieChartConfig;
+}
+
+export const PieChartPreview: FC<PieChartPreviewProps> = ({
+  result,
+  config,
+}) => {
+  const data = useMemo(() => {
+    if (!result?.error) {
+      if (config.key && config.value) {
+        const keyIndex = result.fields.findIndex((key) => key === config.key);
+        const valueIndex = result.fields.findIndex(
+          (key) => key === config.value
+        );
+
+        if (keyIndex !== -1 && valueIndex !== -1) {
+          const total = sumBy(result.values, (arr) =>
+            getFormatValue(arr[valueIndex])
+          );
+
+          return result.values.map((value) => {
+            return {
+              [config.key]: value[keyIndex],
+              [config.value]: getFormatValue(value[valueIndex]),
+              percent: getFormatValue(value[valueIndex]) / total,
+              format: config.format,
+            };
+          });
+        }
+        return [];
+      }
+    }
+
+    return [];
+  }, [config, result]);
+
+  if (!data.length) {
+    return null;
+  }
+
+  return config.variant === "range" ? (
+    <DonutChart
+      tooltip={{
+        fields: [config.key, config.value, "format"],
+        formatter: (item) => ({
+          name: item[config.key],
+          value: getFormatValue(item[config.value], item.format),
+        }),
+      }}
+      label={{
+        formatter: (_, value) =>
+          getFormatValue(value._origin?.[config.value], value._origin?.format),
+      }}
+      statistic={{
+        content: {
+          formatter: (_, values) =>
+            getFormatValue(
+              sumBy(values, (o) => o?.[config.value]),
+              values[0]?.format
+            ),
+        },
+      }}
+      data={data}
+      autoFit
+      radius={0.8}
+      padding="auto"
+      angleField={config.value}
+      colorField={config.key}
+      legend={{ position: "bottom" }}
+      pieStyle={{ stroke: "white", lineWidth: 5 }}
+    />
+  ) : (
+    <Chart
+      padding="auto"
+      scale={{
+        percent: {
+          formatter: (val) => formatPercent(val),
+        },
+      }}
+      autoFit
+      data={data}
+    >
+      <Coordinate type="theta" radius={0.75} />
+      <Tooltip showTitle={false} />
+      <Interval
+        position="percent"
+        adjust="stack"
+        color={config.key}
+        style={{
+          lineWidth: 1,
+          stroke: "#fff",
+        }}
+        label={[
+          "percent",
+          {
+            content: ({ percent }) => formatPercent(percent),
+          },
+        ]}
+      />
+      <Interaction type="element-single-selected" />
+    </Chart>
+  );
+};
