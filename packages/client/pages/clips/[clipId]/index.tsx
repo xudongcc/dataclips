00003644eb6886606d2a9dd531dbfc493e372fe6
chenxuanzong <2622009279@qq.com>
2022-04-14 14:10:45 +0800

    fix: 预览页验证登出状态

--- packages/client/pages/clips/[clipId]/index.tsx
+++ packages/client/pages/clips/[clipId]/index.tsx
@@ -1,53 +1,43 @@
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
 import { Loading } from "../../../components/common/Loading";
 import { Page } from "../../../components/common/Page";
 import { ResultPreview } from "../../../components/clip/ResultPreview";
 import { useQueryResult } from "../../../hooks/useQueryResult";
-import PreviewLayout from "../../../layouts/PreviewLayout";
 import ProjectLayout from "../../../layouts/ProjectLayout";
 import Head from "next/head";
 
 const ClipPreview = () => {
   const router = useRouter();
-  const session = useSession();
 
   const { clipId } = router.query as { clipId: string };
 
   const { data: result, isLoading } = useQueryResult(clipId);
 
-  const content = useMemo(() => {
-    if (isLoading) {
-      return <Loading />;
-    }
-
-    return (
-      <>
-        <Head>
-          <title>{result?.name} - 预览 - 数据集</title>
-        </Head>
-
-        <Page
-          title={result?.name}
-          primaryAction={{
-            text: "编辑",
-            onClick: () => {
-              router.push(`/clips/${clipId}/edit`);
-            },
-          }}
-        >
-          <ResultPreview token={clipId} result={result} />
-        </Page>
-      </>
-    );
-  }, [clipId, isLoading, result]);
-
-  if (session.status === "authenticated") {
-    return <ProjectLayout>{content}</ProjectLayout>;
+  if (isLoading) {
+    return <Loading />;
   }
 
-  return <PreviewLayout>{content}</PreviewLayout>;
+  return (
+    <>
+      <Head>
+        <title>{result?.name} - 预览 - 数据集</title>
+      </Head>
+
+      <Page
+        title={result?.name}
+        primaryAction={{
+          text: "编辑",
+          onClick: () => {
+            router.push(`/clips/${clipId}/edit`);
+          },
+        }}
+      >
+        <ResultPreview token={clipId} result={result} />
+      </Page>
+    </>
+  );
 };
+
+ClipPreview.layout = ProjectLayout;
 
 export default ClipPreview;
--- packages/client/pages/dashboards/[dashboardId]/index.tsx
+++ packages/client/pages/dashboards/[dashboardId]/index.tsx
@@ -1,16 +1,13 @@
 import { PC } from "../../../interfaces/PageComponent";
-import { AdminLayout } from "../../../layouts/AdminLayout/AdminLayout";
 import { useRouter } from "next/router";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   useChartLazyQuery,
   useDashboardQuery,
 } from "../../../generated/graphql";
 import { Layout } from "react-grid-layout";
 import { Loading } from "../../../components/common/Loading";
-import PreviewLayout from "../../../layouts/PreviewLayout";
 import ProjectLayout from "../../../layouts/ProjectLayout";
-import { useSession } from "next-auth/react";
 import { Page } from "../../../components/common/Page";
 import Head from "next/head";
 import {
@@ -27,7 +24,6 @@ interface ChartCard {
 
 const DashboardPreview: PC = () => {
   const router = useRouter();
-  const session = useSession();
 
   const { dashboardId } = router.query as { dashboardId: string };
 
@@ -42,79 +38,64 @@ const DashboardPreview: PC = () => {
     []
   );
 
-  const content = useMemo(() => {
-    if (loading) {
-      return <Loading />;
-    }
-
-    return (
-      <>
-        <Head>
-          <title>{data?.dashboard?.name} - 预览 - 仪表盘</title>
-        </Head>
-
-        <Page
-          title={data?.dashboard?.name}
-          primaryAction={{
-            text: "编辑",
-            onClick: () => {
-              router.push(`/dashboards/${dashboardId}/edit`);
-            },
-          }}
-        >
-          <DashboardLayout
-            type="preview"
-            layout={dragItems.map((item) => ({
-              ...item?.layout,
-              static: true,
-            }))}
-            dragItems={dragItems}
-            cardExtraConfig={{
-              onEditChartClick: (item) => {
-                router.push(`/charts/${item?.chartId}/edit`);
-              },
-              onPreviewClipClick: async (item) => {
-                try {
-                  const { data } = await getChart({
-                    variables: { id: item.chartId },
-                  });
-
-                  if (data?.chart?.clipId) {
-                    router.push(`/clips/${data?.chart?.clipId}`);
-                  }
-                } catch (err) {
-                  console.log("err", err);
-                }
-              },
-              disabledDelete: true,
-              disabledEditCard: true,
-            }}
-          />
-        </Page>
-      </>
-    );
-  }, [
-    dashboardId,
-    data?.dashboard?.name,
-    dragItems,
-    getChart,
-    loading,
-    router,
-  ]);
-
   useEffect(() => {
     if (data?.dashboard?.config?.length) {
       setDragItems(data.dashboard.config);
     }
   }, [data?.dashboard?.config]);
 
-  if (session.status === "authenticated") {
-    return <ProjectLayout>{content}</ProjectLayout>;
+  if (loading) {
+    return <Loading />;
   }
 
-  return <PreviewLayout>{content}</PreviewLayout>;
+  return (
+    <>
+      <Head>
+        <title>{data?.dashboard?.name} - 预览 - 仪表盘</title>
+      </Head>
+
+      <Page
+        title={data?.dashboard?.name}
+        primaryAction={{
+          text: "编辑",
+          onClick: () => {
+            router.push(`/dashboards/${dashboardId}/edit`);
+          },
+        }}
+      >
+        <DashboardLayout
+          type="preview"
+          layout={dragItems.map((item) => ({
+            ...item?.layout,
+            static: true,
+          }))}
+          dragItems={dragItems}
+          cardExtraConfig={{
+            onEditChartClick: (item) => {
+              router.push(`/charts/${item?.chartId}/edit`);
+            },
+            onPreviewClipClick: async (item) => {
+              try {
+                const { data } = await getChart({
+                  variables: { id: item.chartId },
+                });
+
+                if (data?.chart?.clipId) {
+                  router.push(`/clips/${data?.chart?.clipId}`);
+                }
+              } catch (err) {
+                console.log("err", err);
+              }
+            },
+            disabledDelete: true,
+            disabledEditCard: true,
+          }}
+        />
+      </Page>
+    </>
+  );
 };
 
-DashboardPreview.layout = AdminLayout;
+DashboardPreview.layout = ProjectLayout;
 
 export default DashboardPreview;
