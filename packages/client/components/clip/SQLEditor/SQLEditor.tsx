a4428d6f95f072b9f63d85d6c377cc4cd58c3365
chenxuanzong <48152698+chenxuanzong@users.noreply.github.com>
2022-04-21 16:20:49 +0800

    fix: clip sql 语句保存进行格式化 (#125)

--- packages/client/components/clip/SQLEditor/SQLEditor.tsx
+++ packages/client/components/clip/SQLEditor/SQLEditor.tsx
@@ -1,6 +1,7 @@
 import { Box, useColorModeValue, useToken } from "@chakra-ui/react";
 import Editor, { loader } from "@monaco-editor/react";
-import { FC, useCallback } from "react";
+import { FC, useCallback, useEffect, useRef } from "react";
+import { format } from "prettier-sql";
 
 loader.config({
   paths: { vs: "/editor" },
@@ -8,11 +9,18 @@ loader.config({
 
 export interface SQLEditorProps {
   value: string;
+  formatType?: "mysql" | "postgresql";
   onChange: (value: string) => void;
 }
 
-export const SQLEditor: FC<SQLEditorProps> = ({ value, onChange }) => {
+export const SQLEditor: FC<SQLEditorProps> = ({
+  value,
+  onChange,
+  formatType = "mysql",
+}) => {
   const theme = useColorModeValue("dataclips-light", "dataclips-dark");
+  const editorRef = useRef(null);
+
   const [
     lightBackground,
     lightLineHighlightBackground,
@@ -57,6 +65,41 @@ export const SQLEditor: FC<SQLEditorProps> = ({ value, onChange }) => {
     ]
   );
 
+  // 监听保存按键，格式化 sql 语句
+  useEffect(() => {
+    const handleSqlFormat = (e) => {
+      const formatValue = () => {
+        e.preventDefault();
+
+        onChange(
+          format(editorRef.current?.getValue(), { language: formatType })
+        );
+      };
+
+      // 焦点建立在 TextArea 的情况下
+      if (document.activeElement.tagName === "TEXTAREA") {
+        // MAC 平台
+        if (window.navigator.platform.match("Mac")) {
+          if (e.metaKey && e.code === "KeyS") {
+            formatValue();
+          }
+          // 其他平台
+        } else {
+          if (e.ctrlKey && e.code === "KeyS") {
+            formatValue();
+          }
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleSqlFormat);
+
+    return () => {
+      window.removeEventListener("keydown", handleSqlFormat);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formatType]);
+
   return (
     <Box h="200px">
       <Editor
@@ -75,6 +118,9 @@ export const SQLEditor: FC<SQLEditorProps> = ({ value, onChange }) => {
         }}
         value={value}
         onChange={handleChange}
+        onMount={(editor) => {
+          editorRef.current = editor;
+        }}
         beforeMount={handleBeforeMount}
       />
     </Box>
